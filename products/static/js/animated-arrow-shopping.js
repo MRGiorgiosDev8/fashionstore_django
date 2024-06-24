72a7d433b1de608c8447c478c00508400471845f
Georgio <georgetrueman64@gmail.com>
2024-06-25 02:52:29 +0300

    new

---
+++ products/static/js/animated-arrow-shopping.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const arrowShopping = document.getElementById('arrow-shopping');
+
+    gsap.set(arrowShopping, { x: 14,  });
+
+    const arrowAnimation = gsap.timeline({ repeat: -1, yoyo: true });
+    arrowAnimation.to(arrowShopping, {
+        x: -5, 
+        duration: 0.8, 
+        ease: 'power1.inOut' 
+    }).to(arrowShopping, {
+        x: 7, 
+        duration: 0.8, 
+        ease: 'power1.inOut' 
+    });
+});
