f2cd43c28d87d428761a41273fb764040471a1e9
Georgio <georgetrueman64@gmail.com>
2024-06-27 01:21:25 +0300

    update

--- products/static/js/categoty-animated.js
+++ products/static/js/categoty-animated.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     const listGroup = document.querySelector('.list-group');
+    const links = listGroup.querySelectorAll('a');
 
-    gsap.set(listGroup, { y: -100, opacity: 0 });
+    if (!sessionStorage.getItem('linkClicked')) {
+        gsap.set(listGroup, { y: -100, opacity: 0 });
 
-    gsap.to(listGroup, {
-        y: 0,
-        opacity: 1,
-        duration: 1.5,
-        ease: 'power2.out'
+        gsap.to(listGroup, {
+            y: 0,
+            opacity: 1,
+            duration: 1.5,
+            ease: 'power2.out'
+        });
+    }
+
+    links.forEach(link => {
+        link.addEventListener('click', function () {
+            sessionStorage.setItem('linkClicked', 'true');
+        });
     });
+
+    setTimeout(() => {
+        sessionStorage.removeItem('linkClicked');
+    }, 1000);
 });
