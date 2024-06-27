be7959c418c23e92029f133e09751fafa0047d20
Georgio <georgetrueman64@gmail.com>
2024-06-27 14:29:15 +0300

    update

--- products/static/js/animated-arrow-shopping.js
+++ products/static/js/animated-arrow-shopping.js
@@ -1,16 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const arrowShopping = document.getElementById('arrow-shopping');
 
-    gsap.set(arrowShopping, { x: 14,  });
+    gsap.set(arrowShopping, { x: 14 });
 
     const arrowAnimation = gsap.timeline({ repeat: -1, yoyo: true });
     arrowAnimation.to(arrowShopping, {
-        x: -5, 
-        duration: 0.8, 
-        ease: 'power1.inOut' 
+        x: -5,
+        duration: 0.8,
+        ease: 'power1.inOut'
     }).to(arrowShopping, {
-        x: 7, 
-        duration: 0.8, 
-        ease: 'power1.inOut' 
+        x: 7,
+        duration: 0.8,
+        ease: 'power1.inOut'
+    });
+
+    arrowShopping.addEventListener('mouseenter', function () {
+        gsap.to(arrowShopping, { scale: 1.1, duration: 0.3 });
+    });
+
+    arrowShopping.addEventListener('mouseleave', function () {
+        gsap.to(arrowShopping, { scale: 1, duration: 0.3 });
     });
 });
