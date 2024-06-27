d6fae49de5916f3f5a1af048936fa8597066728a
Georgio <georgetrueman64@gmail.com>
2024-06-27 22:00:19 +0300

    new

---
+++ products/static/js/animated-card.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const card = document.querySelector('.card');
+    gsap.set(card, { opacity: 0, y: -30 });
+
+    gsap.to(card, {
+        y: 0,
+        opacity: 1,
+        duration: 1.1,
+        ease: 'power2.inOut'
+    });
+});
