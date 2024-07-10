1e5003ffbb075f7a4a8a90e4bfa7af0e9a277eed
Georgio <georgetrueman64@gmail.com>
2024-07-10 16:46:23 +0300

    update

--- products/static/js/click_img.js
+++ products/static/js/click_img.js
@@ -1,8 +1,6 @@
 function changeImage(element) {
-    // Получаем URL маленького изображения
     let smallImgUrl = element.src;
-    // Находим большое изображение
     let bigImg = document.getElementById('bigImg');
-    // Меняем URL большого изображения на URL маленького
+
     bigImg.src = smallImgUrl;
 }
